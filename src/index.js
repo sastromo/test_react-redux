dc19894c4fa420a2baefa2f50d629ad3fef21972
Andre Sa <andre@esterisco.com>
2020-03-07 12:57:46 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 import { createStore } from 'redux';
-import allReducers from './reducers';
+import allReducers from './store/reducers/index';
 import { Provider } from 'react-redux';
 
 const myStore = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
